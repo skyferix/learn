909aac1731631c5d187768c470ff6e7819f7df7a
Skyferix <patrycjusz.august@gmail.com>
2021-11-12 20:41:06 +0200

    crazy stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <Header title="1 header" />
     </div>
   );
 }
